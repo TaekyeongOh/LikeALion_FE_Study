d5364621812413eb7e4d0b6d1f02fec895f4b33d
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-05-29 20:55:01 +0900

    0529

---
+++ 0529/week4/src/App.js
@@ -0,0 +1,15 @@
+//App.js
+
+import React from 'react';
+import Naver from './Naver';
+import '../src/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Naver />
+    </div>
+  );
+}
+
+export default App;
